d9d3a03e11226801133dbe1c8da2dc51b9c973e8
AshutoshDhumal11 <ashutoshcode04@gmail.com>
2024-10-07 12:32:07 +0530

    Database and Payment Connection

---
+++ config/cloudinary.js
@@ -0,0 +1,18 @@
+const cloudinary = require("cloudinary").v2;
+
+// Cloudinary connection for uploading media files.
+
+exports.cloudinaryConnect = () => {
+	try {
+		cloudinary.config({
+			cloud_name: process.env.CLOUD_NAME,
+			api_key: process.env.API_KEY,
+			api_secret: process.env.API_SECRET,
+		});
+
+		console.log("Cloudinary connection successful");
+	} catch (error) {
+		console.log(error);
+		console.log("Cloudinary connection failed");
+	}
+};
