4594cb80fb059aa6f44dd88989732bd98f2690d7
AshutoshDhumal11 <ashutoshcode04@gmail.com>
2024-10-07 12:36:35 +0530

    Define contact and payment controller

---
+++ controllers/ContactUs.js
@@ -0,0 +1,31 @@
+const { contactUsEmail } = require("../mail/templates/contactFormRes");
+const mailSender = require("../utils/mailSender")
+
+exports.contactUsController = async (req, res) => {
+
+    // fetch data from req body
+    const { email, firstname, lastname, message, phoneNo, countrycode } = req.body
+    console.log(req.body);
+
+    try {
+        const emailResponse = await mailSender(
+            email,
+            "Your Data send successfully",
+            contactUsEmail(email, firstname, lastname, message, phoneNo, countrycode)
+        )
+        console.log("Email response is ", emailResponse);
+
+        // return 
+        return res.status(200).json({
+            success: true,
+            message: "Email send successfully",
+        });
+
+    } catch (error) {
+        console.log("Error message :", error.message)
+        return res.status(200).json({
+            success: false,
+            message: "Something went wrong while sending email to StudyNotion",
+        })
+    }
+}
