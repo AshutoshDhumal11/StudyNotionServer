e4cf92c7642fc5eacf46dd1fe1254844723d4787
AshutoshDhumal11 <ashutoshcode04@gmail.com>
2025-10-05 12:10:50 +0530

    Update sendotp file

--- models/OTP.js
+++ models/OTP.js
@@ -26,8 +26,7 @@ async function sendVerificationEmail(email, otp) {
       "Verification Email",
       emailTemplate(otp)
     );
-
-    console.log("Email sent successfully: ", mailResponse.response);
+c
   } catch (error) {
     console.log("Error occurred while sending email: ", error);
     throw error;
