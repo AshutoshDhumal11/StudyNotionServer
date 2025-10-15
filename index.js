6dae37e139bc4aa2b999b0d59fefe393d224f033
AshutoshDhumal11 <ashutoshcode04@gmail.com>
2025-10-15 20:02:22 +0530

    Updated cors issue

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://study-notion-eta-nine.vercel.app/",
+    origin: "https://study-notion-eta-nine.vercel.app",
     credentials: true,
   })
 );
